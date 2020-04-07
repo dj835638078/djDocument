e20c5199307858028ddc2e5b9e03822b9a7bbcda
dj <jiandeng@isoftstone.com>
2020-04-07 17:29:03 +0800

    dj

--- code/dj-react/react-test/src/TodoItem.js
+++ code/dj-react/react-test/src/TodoItem.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-function TodoItem(props) {
-    const { content, deleteItem, test } = props
-    return (
-        <li onClick={deleteItem}>{content}-{test}</li>
-    )
+// function TodoItem(props) {
+//     const { content, deleteItem, test } = props
+//     return (
+//         <li onClick={deleteItem}>{content}-{test}</li>
+//     )
+// }
+class TodoItem extends Component {
+    constructor(props) {
+        super(props)
+    }
+    shouldComponentUpdate(nextProps, nextStates) {
+        if (nextProps.content !== this.props.content) {
+            return true
+        }
+        return false
+    }
+    handleClick = () => {
+        const { deleteItem, index } = this.props
+        deleteItem(index)
+    }
+    render() {//deleteItem
+        const { content, test } = this.props
+        return (
+            <li onClick={this.handleClick}>{content}-{test}</li>
+        )
+    }
 }
 TodoItem.propTypes = { //设置父组件传递过来的参数的属性string/number/array...
     test: PropTypes.string.isRequired,//必填
-    content: PropTypes.arrayOf(PropTypes.string, PropTypes.number),//content可以是数字或者是字符串
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),//content可以是数字或者是字符串
     deleteItem: PropTypes.func
 }
 TodoItem.defaultProps = { //设置父组件传递过来的参数的默认值
--- code/dj-react/react-test/src/TodoList.js
+++ code/dj-react/react-test/src/TodoList.js
@@ -1,21 +1,26 @@
 import React, { Component, Fragment } from 'react'; //jsx语法中一定要由一个大标签包裹，Fragment为一个标签占用符，
 import './todoList.css'
 import TodoItem from './TodoItem'
+import store from './store'
 
 class TodoList extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            inputValue: "",
-            listData: []
-        }
+        // this.state = {
+        //     inputValue: "",
+        //     listData: []
+        // }
+        this.state = store.getState()
+        this.handleStoreChange = this.handleStoreChange.bind(this)
+        store.subscribe(this.handleStoreChange)
     }
     render() {
         const { inputValue } = this.state
         return (
             <Fragment>
                 <div>
-                    <input type="text" className='input' value={inputValue} onChange={this.handleInputChange} /><button onClick={this.handleBtnClick}>提交</button>
+                    <input type="text" className='input' value={inputValue} onChange={this.handleInputChange} />
+                    <button ref={(btn) => { this.btnFlag = btn }} onClick={this.handleBtnClick}>提交</button>
                 </div>
                 <ul>
                     {this.getTodoLIst()}
@@ -23,21 +28,32 @@ class TodoList extends Component {
             </Fragment >
         )
     }
+
+    handleStoreChange() {
+        debugger
+        console.log(store.getState())
+        this.setState(
+            store.getState()
+        )
+    }
+
     getTodoLIst = () => {
         return this.state.listData.map((item, index) => {
             // return <li key={index} onClick={() => this.handleDelClick(index)}>{item}</li>
-            return <TodoItem key={index} content={item} deleteItem={() => this.handleDelClick(index)} />
+            return <TodoItem key={index} content={item} index={index} deleteItem={() => this.handleDelClick(index)} />
         })
     }
 
     handleInputChange = (e) => {
-        // this.setState({
-        //     inputValue: e.target.value
-        // })
-        const inputValue = e.target.value
-        this.setState(() => ({
-            inputValue
-        }))
+        // const inputValue = e.target.value
+        // this.setState(() => ({
+        //     inputValue
+        // }))
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action)
     }
 
     handleBtnClick = () => {
@@ -45,6 +61,7 @@ class TodoList extends Component {
         //     listData: [...this.state.listData, this.state.inputValue],
         //     inputValue: ""
         // })
+        // console.log(this.btnFlag)//使用ref
         this.setState((preState) => ({
             listData: [...preState.listData, preState.inputValue],
             inputValue: ""
